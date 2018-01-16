dd4c8ac45421dc35432c7680876570e080a42c7f
Andrey Isaev <isaevdev@gmail.com>
2018-01-16 23:20:42 +0300

    minor fix

--- NeutronGeometry/ViewController.swift
+++ NeutronGeometry/ViewController.swift
@@ -77,16 +77,16 @@ class ViewController: NSViewController {
         
         // Layer 1
         var paddingAngle: CGFloat = 0 // Смещение угла относительно предыдущего ряда счетчиков (нужно по-максимуму закрыть промежутки между счетчиками чтобы увеличить эффективность)
-        addCountersLayer(tag: 1, total: total1, paddingAngle: paddingAngle, countersLayerCenterRadius: countersLayerCenterRadius1, color: NSColor.green)
+        addCountersLayer(tag: 1, total: total1, paddingAngle: paddingAngle, countersLayerCenterRadius: countersLayerCenterRadius1)
         // Layer 2
         paddingAngle += (CGFloat.pi * 2 * CGFloat(1)/CGFloat(total1)) / 2
-        addCountersLayer(tag: 2, total: total2, paddingAngle: paddingAngle, countersLayerCenterRadius: countersLayerCenterRadius2, color: NSColor.red)
+        addCountersLayer(tag: 2, total: total2, paddingAngle: paddingAngle, countersLayerCenterRadius: countersLayerCenterRadius2)
         // Layer 3
         paddingAngle += (CGFloat.pi * 2 * CGFloat(1)/CGFloat(total2)) / 2
-        addCountersLayer(tag: 3, total: total3, paddingAngle: paddingAngle, countersLayerCenterRadius: countersLayerCenterRadius3, color: NSColor.blue)
+        addCountersLayer(tag: 3, total: total3, paddingAngle: paddingAngle, countersLayerCenterRadius: countersLayerCenterRadius3)
     }
     
-    fileprivate func addCountersLayer(tag: Int, total: Int, paddingAngle: CGFloat, countersLayerCenterRadius: CGFloat, color: NSColor) {
+    fileprivate func addCountersLayer(tag: Int, total: Int, paddingAngle: CGFloat, countersLayerCenterRadius: CGFloat) {
         let bounds = self.view.bounds
         for i in 0...total-1 {
             let counterIndex = counters.count
@@ -109,9 +109,8 @@ class ViewController: NSViewController {
             counter.wantsLayer = true
             counter.layer?.cornerRadius = counterRadius/2
             counter.layer?.masksToBounds = true
+            let color = presure == .high ? NSColor.blue : NSColor.red
             counter.layer?.backgroundColor = color.cgColor
-            counter.layer?.borderColor = NSColor.brown.cgColor
-            counter.layer?.borderWidth = presure == .low ? 2 : 0
             
             let label = NSTextField(frame: NSRect(x: 0, y: 0, width: frame.width, height: frame.height))
             label.isBezeled = false
