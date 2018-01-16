923d0126b7a15f32ed3e3727ae0d7e4a23eec144
Andrey Isaev <isaevdev@gmail.com>
2018-01-16 16:08:20 +0300

    Initial Commit

---
+++ NeutronGeometry/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NeutronGeometry
+//
+//  Created by Andrey Isaev on 16/01/2018.
+//  Copyright © 2018 Andrey Isaev. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ NeutronGeometry/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NeutronGeometry
+//
+//  Created by Andrey Isaev on 16/01/2018.
+//  Copyright © 2018 Andrey Isaev. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
