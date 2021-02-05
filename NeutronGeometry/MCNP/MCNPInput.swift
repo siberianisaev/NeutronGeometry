733da6811e73e7c587d5cf32814a338cebd96de2
Andrey Isaev <isaevdev@gmail.com>
2021-02-05 23:24:46 +0300

    Update MCNPInput.swift

--- NeutronGeometry/MCNP/MCNPInput.swift
+++ NeutronGeometry/MCNP/MCNPInput.swift
@@ -222,9 +222,6 @@ c ==== CELLS =====
         let overalCoefficient = overalTallyCoefficient(layers).stringWith(precision: 6)
         var result = """
 \nc ---------------- TALLY ------------
-\nc ----- Scintillator ------------
-F4:N 10005
-FM4 (1 5 \(naReactionId))
 \nc ----- He3-counters ------------
 F4:N \(firstCounterCellId) \(totalDetectorsCount-2)i \(lastCounterCellId) (\(firstCounterCellId) \(totalDetectorsCount-2)i \(lastCounterCellId))
 FM4 (\(overalCoefficient) 3 \(npReactionId))
